3901b8a80915af030389576314b13a657a90871e
Franco Cancino <francocancino0@gmail.com>
2024-10-21 12:14:14 -0300

    Agrege rama de desarrollo, borrando las dos lineas que estaban en el main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
-        System.out.println("Prueba12...");
+
+        System.out.println("Probando la rama de desarrollo...");
+
     }
 }
