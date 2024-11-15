763e0866dc38c962ba09fe14efb4d886a37f045a
SofiaCantalupi <sofiacantalupi@gmail.com>
2024-11-14 22:09:57 -0300

    creacion clase Sombrilla

---
+++ src/servicio/Sombrilla.java
@@ -0,0 +1,14 @@
+package servicio;
+
+public class Sombrilla extends Servicio {
+
+    // Constructores
+    public Sombrilla() {
+    }
+
+    public Sombrilla(int id, boolean ocupado, int capacidad, double precio) {
+        super(id, ocupado, capacidad, precio);
+    }
+
+
+}
