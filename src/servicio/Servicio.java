f2bbe31b994a11dfc4f55bf2503bead96f62a3d2
SofiaCantalupi <sofiacantalupi@gmail.com>
2024-11-14 22:05:43 -0300

    creacion clase Servicio

---
+++ src/servicio/Servicio.java
@@ -0,0 +1,81 @@
+package servicio;
+
+import java.util.Objects;
+
+public class Servicio {
+    private int id;
+    private boolean ocupado;
+    private int capacidad;
+    private double precio;
+
+    //Constructores
+
+    public Servicio() {
+    }
+
+    public Servicio(int id, boolean ocupado, int capacidad, double precio) {
+        this.id = id;
+        this.ocupado = ocupado;
+        this.capacidad = capacidad;
+        this.precio = precio;
+    }
+
+    //Setters / Getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // to String
+
+    @Override
+    public String toString() {
+        return "Servicio{" +
+                "id=" + id +
+                ", ocupado=" + ocupado +
+                ", capacidad=" + capacidad +
+                ", precio=" + precio +
+                '}';
+    }
+
+    // Equals / Hashcode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Servicio servicio)) return false;
+        return id == servicio.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
