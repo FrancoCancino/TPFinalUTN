26321d4dd2875a03fd14c53c525c38f53c01efd2
SofiaCantalupi <sofiacantalupi@gmail.com>
2024-11-19 14:45:25 -0300

    agregacion de atributo prioritario en PlazaEstacionamiento

--- src/servicio/clases/PlazaEstacionamiento.java
+++ src/servicio/clases/PlazaEstacionamiento.java
@@ -1,12 +1,31 @@
 package servicio.clases;
 
 public class PlazaEstacionamiento extends Servicio {
+    private boolean prioritario;
 
     // Constructores
     public PlazaEstacionamiento() {
     }
 
-    public PlazaEstacionamiento(int id, boolean ocupado, int capacidad, double precio) {
+    public PlazaEstacionamiento(int id, boolean ocupado, int capacidad, double precio, boolean prioritario) {
         super(id, ocupado, capacidad, precio);
+        this.prioritario = prioritario;
+    }
+
+    //Setters / getters
+    public boolean getIsPrioritario() {
+        return prioritario;
+    }
+
+    public void setPrioritario(boolean prioritario) {
+        this.prioritario = prioritario;
+    }
+
+    //to String
+    @Override
+    public String toString() {
+        return "PlazaEstacionamiento{" + super.toString() +
+                "prioritario=" + prioritario +
+                '}';
     }
 }
