8e223935bc7140c5703e59828ffc20052bbf1f1e
SofiaCantalupi <sofiacantalupi@gmail.com>
2024-11-21 19:02:16 -0300

    cambios en constructor y toString

--- src/servicio/clases/PlazaEstacionamiento.java
+++ src/servicio/clases/PlazaEstacionamiento.java
@@ -1,35 +1,23 @@
 package servicio.clases;
 
 public class PlazaEstacionamiento extends Servicio {
-    private boolean prioritario;
 
     private static int cantidadPlazas = 0;
     private static final double PRECIO_ACTUAL = 8000;
 
     // Constructores
     public PlazaEstacionamiento() {
-    }
-
-    public PlazaEstacionamiento(boolean prioritario) {
-        this.setId(++cantidadPlazas);
+        ++cantidadPlazas;
+        this.setId("PE-" + cantidadPlazas);
         this.setPrecio(PRECIO_ACTUAL);
-        this.prioritario = prioritario;
     }
 
-    //Setters / getters
-    public boolean getIsPrioritario() {
-        return prioritario;
-    }
-
-    public void setPrioritario(boolean prioritario) {
-        this.prioritario = prioritario;
-    }
-
-    //to String
+    // toString
     @Override
     public String toString() {
-        return "PlazaEstacionamiento{" + super.toString() +
-                "prioritario=" + prioritario +
-                '}';
+        return  this.getId() +
+                " Ocupado= " + this.getOcupado() +
+                " Precio= " + this.getPrecio()
+                ;
     }
 }
